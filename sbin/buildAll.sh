0694c42c1f00ddc4e9862333e22c695479aba554
jutorres <juliana.torres@thoughtworks.com>
2020-06-02 18:26:54 -0300

    [TWDU#1] [@jutorres @dmsantostw] Update build file to add test

--- sbin/buildAll.sh
+++ sbin/buildAll.sh
@@ -4,6 +4,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 echo "====Building Producer JARs===="
 $DIR/../CitibikeApiProducer/gradlew -p $DIR/../CitibikeApiProducer clean bootJar
+$DIR/../CitibikeApiProducer/gradlew -p $DIR/../CitibikeApiProducer gradle test
 echo "====Building Consumer JARs===="
 cd $DIR/../RawDataSaver && sbt test && sbt package
 cd $DIR/../StationConsumer && sbt test && sbt package
