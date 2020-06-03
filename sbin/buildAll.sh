31b08a823409b120b529223f96ad3b8f77614563
jutorres <juliana.torres@thoughtworks.com>
2020-06-03 11:53:24 -0300

    [TWDU#1] [@jutorres @dmsantostw] Update buildAll to run test to CitibikeApiProducer

--- sbin/buildAll.sh
+++ sbin/buildAll.sh
@@ -4,7 +4,8 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 echo "====Building Producer JARs===="
 $DIR/../CitibikeApiProducer/gradlew -p $DIR/../CitibikeApiProducer clean bootJar
+$DIR/../CitibikeApiProducer/gradlew -p $DIR/../CitibikeApiProducer test
 echo "====Building Consumer JARs===="
-cd $DIR/../RawDataSaver && sbt package
-cd $DIR/../StationConsumer && sbt package
-cd $DIR/../StationTransformerNYC && sbt package
+cd $DIR/../RawDataSaver && sbt test && sbt package
+cd $DIR/../StationConsumer && sbt test && sbt package
+cd $DIR/../StationTransformerNYC && sbt test && sbt package
