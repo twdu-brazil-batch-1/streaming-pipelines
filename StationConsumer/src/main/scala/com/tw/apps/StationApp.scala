68227a9f01d45371f9d497ef45d523874e1abf5d
Luan Andrade <luansandrade92@hotmail.com>
2020-06-05 16:23:08 -0300

    [TWDU#25] [@lsandrade @3jacksonsmith @cristhianetais] Save only SF stations

--- StationConsumer/src/main/scala/com/tw/apps/StationApp.scala
+++ StationConsumer/src/main/scala/com/tw/apps/StationApp.scala
@@ -54,10 +54,10 @@ object StationApp {
 
     sfStationDF
 //      .union(sfStationDF)
-      .as[StationData]
-      .groupByKey(r=>r.station_id)
-      .reduceGroups((r1,r2)=>if (r1.last_updated > r2.last_updated) r1 else r2)
-      .map(_._2)
+//      .as[StationData]
+//      .groupByKey(r=>r.station_id)
+//      .reduceGroups((r1,r2)=>if (r1.last_updated > r2.last_updated) r1 else r2)
+//      .map(_._2)
       .writeStream
       .format("overwriteCSV")
       .option("failOnDataLoss", false)
