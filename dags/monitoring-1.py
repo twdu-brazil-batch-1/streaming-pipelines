c44004037758054f39c45d6da0e562e7cc3cc552
Nayara Souza <nsouza@thoughtworks.com>
2020-06-08 14:20:47 -0300

    10# - @nsouza @jutorres - Removes SSHHook operator

--- dags/monitoring-1.py
+++ dags/monitoring-1.py
@@ -3,7 +3,6 @@ import subprocess
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
 from airflow.operators.bash_operator import BashOperator
-from airflow.contrib.hooks import SSHHook
 
 from datetime import datetime, timedelta
 
@@ -19,12 +18,12 @@ default_args = {
     'retry_delay': timedelta(minutes=5),
 }
 
-sshHook = SSHHook(conn_id="emr-master.twdu-brazil-batch-1.training")
-
 dag = DAG('monitoring_CSV_1', default_args=default_args)
 
 read_csv_cmd = """
 export AWS_DEFAULT_REGION=us-east-2
+
+ssh emr-master.twdu-brazil-batch-1.training
 
 echo =======GET CREATION TIME========
 
@@ -35,10 +34,9 @@ echo csv_create_time
 echo =====COMPARE DATES========
 """
 
-read_csv_task = SSHExecuteOperator(
+read_csv_task = BashOperator(
     task_id="read_csv_file",
     bash_command=read_csv_cmd,
-    ssh_hook=sshHook,
     dag=dag)
 
 read_csv_task
