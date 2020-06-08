96b994b79ac94fd181e622e124ad10e7ac81c87d
Nayara Souza <nsouza@thoughtworks.com>
2020-06-08 13:55:03 -0300

    10# - @nsouza @jutorres - Use SSHExecutorOperator in DAG

--- dags/monitoring-1.py
+++ dags/monitoring-1.py
@@ -3,6 +3,7 @@ import subprocess
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
 from airflow.operators.bash_operator import BashOperator
+from airflow.contrib.hooks import SSHHook
 
 from datetime import datetime, timedelta
 
@@ -18,28 +19,26 @@ default_args = {
     'retry_delay': timedelta(minutes=5),
 }
 
+sshHook = SSHHook(conn_id="emr-master.twdu-brazil-batch-1.training")
 
 dag = DAG('monitoring_CSV_1', default_args=default_args)
-
 
 read_csv_cmd = """
 export AWS_DEFAULT_REGION=us-east-2
 
-echo ========SSH EMR========
-ssh emr-master.twdu-brazil-batch-1.training
-
 echo =======GET CREATION TIME========
 
-export csv_create_time=hadoop fs -stat "%y" /tw/stationMart/data/_SUCCESS
+csv_create_time='hadoop fs -stat "%y" /tw/stationMart/data/_SUCCESS'
 
-echo $csv_create_time
+echo csv_create_time
 
 echo =====COMPARE DATES========
 """
 
-read_csv_task = BashOperator(
-    task_id='read_csv_task',
+read_csv_task = SSHExecuteOperator(
+    task_id="read_csv_file",
     bash_command=read_csv_cmd,
+    ssh_hook=sshHook,
     dag=dag)
 
 read_csv_task
