95c65ff03f5633076baf9db45a86b09ecb1496e0
Jackson Matias <jsmatias@thoughtworks.com>
2020-06-09 16:15:29 -0300

    10# - @nsouza, @3jacksonsmith - Create a function to transform timestamp to unix timestamp value

--- dags/monitoring.py
+++ dags/monitoring.py
@@ -22,12 +22,28 @@ def read_csv_timestamp():
       
       echo $TIMESTAMP
     """
+def convert_to_unix(input_timestamp):
+    input_timestamp_split = input_timestamp.split(" ")
+
+    date_split = input_timestamp_split[0]
+    date_list = date.split("-")
+    convert_date_to_int = list(map(int, date_list))
+
+    hour = input_timestamp_split[1]
+    hour_list = hour.split(":")
+    convert_hour_to_int = list(map(int, hour_list))
+
+    date_converted = date(convert_date_to_int[0], convert_date_to_int[1], convert_date_to_int[2])
+    hour_converted = datetime.time(convert_hour_to_int[0], convert_hour_to_int[1], convert_hour_to_int[2])
+
+    final_date_converted = datetime.datetime.combine(date_converted, hour_converted)
+
+    return final_date_converted.strftime('%s')
 
 def is_csv_updated(**context):
     last_csv_update = context['task_instance'].xcom_pull(task_ids='read_csv_file', key='return_value')
     print("CSV file was last updated at", last_csv_update)
     return last_csv_update
-
 
 with DAG('CSV_monitor_1', 
     default_args=default_args,
