77fd7608787a04b350787a6e0a81bc94b6b51f1a
Luan Andrade <luansandrade92@hotmail.com>
2020-06-09 17:30:41 -0300

    [TWDU#25][@lsandrade, @dmsantostw] Fix function kill_process

--- sbin/deploy.sh
+++ sbin/deploy.sh
@@ -64,7 +64,7 @@ set -e
 
 function kill_process {
     query=\$1
-    pid=`ps aux | grep \$query | grep -v "grep" |  awk "{print \\\$2}"`
+    pid=`ps aux | grep \$query | grep -v "grep" |  awk "{print \$2}"`
 
     if [ -z "\$pid" ];
     then
